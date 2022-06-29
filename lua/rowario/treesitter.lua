817e3f5a17c0b638bafba0e0e6c281cafb953af6
rowario <r0wario@ya.ru>
2022-06-29 14:30:28 +0300

    added fish script to treesitter

--- lua/rowario/treesitter.lua
+++ lua/rowario/treesitter.lua
@@ -1,5 +1,5 @@
 require'nvim-treesitter.configs'.setup {
-	ensure_installed = { "typescript", "lua", "rust", "php", "c_sharp" },
+	ensure_installed = { "typescript", "lua", "rust", "php", "c_sharp", "fish" },
 	sync_install = true,
 	highlight = {
 		enable = true,
